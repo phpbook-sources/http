2b2b8abc8c9e4c5c12850caa708abcde8369cd84
PHPBook <phpbook@programmer.net>
2018-07-19 08:54:29 -0300

    Proxy Loader and Documentation

--- assets/html/docs-body-resources.php
+++ assets/html/docs-body-resources.php
@@ -9,85 +9,93 @@
             
         <p>
 
-            <?php foreach (\PHPBook\Http\Request::getResources() as $key => $resource): ?>
+            <?php if (count(\PHPBook\Http\Request::getResources()) > 0): ?>
 
-                <div class="subtitle">
-                    <?php echo $resource->getUri(); ?>
-                </div>
+                <?php foreach (\PHPBook\Http\Request::getResources() as $key => $resource): ?>
 
-                <p>
-                    <strong><?php echo $resource->getNotes(); ?></strong>
-                </p>
-                
-                <div class="fieldset">
-                    <div class="name">
-                        <?php echo strtoupper($resource->getType()); ?>
+                    <div class="subtitle">
+                        <?php echo $resource->getUri(); ?>
                     </div>
-                    <div class="data">
-                        <span class="label-info"><?php echo $path . \PHPBook\Http\Configuration\Directory::getApp() . '/'; ?></span><span class="label-important"><?php echo $resource->getUri(); ?></span>
-                    </div>							
-                </div>
 
-                <?php if ($resource->getInputUri()): ?>
+                    <p>
+                        <strong><?php echo $resource->getNotes(); ?></strong>
+                    </p>
+
                     <div class="fieldset">
                         <div class="name">
-                            URI Input
+                            <?php echo strtoupper($resource->getType()); ?>
                         </div>
                         <div class="data">
-                            <xmp><?php
-                                list($type, $element, $rules) = $resource->getInputUri();
-                                $inputUri = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
-                                echo \PHPBook\Http\Dispatch::schema($inputUri->schema());?></xmp>
+                            <span class="label-info"><?php echo $path . \PHPBook\Http\Configuration\Directory::getApp() . '/'; ?></span><span class="label-important"><?php echo $resource->getUri(); ?></span>
                         </div>							
                     </div>
-                <?php endif; ?>
 
-                <?php if ($resource->getInputBody()): ?>
+                    <?php if ($resource->getInputUri()): ?>
+                        <div class="fieldset">
+                            <div class="name">
+                                URI Input
+                            </div>
+                            <div class="data">
+                                <xmp><?php
+                                    list($type, $element, $rules) = $resource->getInputUri();
+                                    $inputUri = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
+                                    echo \PHPBook\Http\Dispatch::schema($inputUri->schema());?></xmp>
+                            </div>							
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if ($resource->getInputBody()): ?>
+                        <div class="fieldset">
+                            <div class="name">
+                                Body Input
+                            </div>
+                            <div class="data">
+                                <xmp><?php
+                                    list($type, $element, $rules) = $resource->getInputBody();
+                                    $inputBody = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
+                                    echo \PHPBook\Http\Dispatch::schema($inputBody->schema());?></xmp>
+                            </div>							
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if ($resource->getOutput()): ?>
+
+                        <div class="fieldset">
+                            <div class="name">
+                                Output
+                            </div>
+                            <div class="data">
+                                <xmp><?php echo \PHPBook\Http\Dispatch::schema(\PHPBook\Http\Configuration\Output::getContent());?>
+                                </xmp>
+                                <strong>where output @ is</strong>
+                                <xmp><?php
+                                    list($type, $element, $rules) = $resource->getOutput();
+                                    $output = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
+                                    echo \PHPBook\Http\Dispatch::schema($output->schema());?></xmp>                            
+                            </div>						
+                        </div>
+
+                    <?php endif; ?>
+
                     <div class="fieldset">
                         <div class="name">
-                            Body Input
+                            Output Exception
                         </div>
                         <div class="data">
-                            <xmp><?php
-                                list($type, $element, $rules) = $resource->getInputBody();
-                                $inputBody = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
-                                echo \PHPBook\Http\Dispatch::schema($inputBody->schema());?></xmp>
-                        </div>							
+                            <xmp><?php echo \PHPBook\Http\Dispatch::schema(\PHPBook\Http\Configuration\Output::getException()); ?></xmp>
+                            <strong>where ouput exception @ is equal the string message</strong>
+                        </div>	
                     </div>
-                <?php endif; ?>
+                            
+                    <hr />
 
-                <?php if ($resource->getOutput()): ?>
+                    <?php endforeach; ?>
 
-                     <div class="fieldset">
-                        <div class="name">
-                            Output
-                        </div>
-                        <div class="data">
-                            <xmp><?php echo \PHPBook\Http\Dispatch::schema(\PHPBook\Http\Configuration\Output::getContent());?>
-                            </xmp>
-                            <strong>where output @ is</strong>
-                            <xmp><?php
-                                list($type, $element, $rules) = $resource->getOutput();
-                                $output = new \PHPBook\Http\Query(new $type($element, 'Output'), $rules);
-                                echo \PHPBook\Http\Dispatch::schema($output->schema());?></xmp>                            
-                        </div>						
-                    </div>
+            <?php else: ?>
 
-                <?php endif; ?>
-                
-                <div class="fieldset">
-                    <div class="name">
-                        Output Exception
-                    </div>
-                    <div class="data">
-                        <xmp><?php echo \PHPBook\Http\Dispatch::schema(\PHPBook\Http\Configuration\Output::getException()); ?></xmp>
-                        <strong>where ouput exception @ is equal the string message</strong>
-                    </div>	
-                </div>
-                        
-                <hr />
+                <strong>There is no resources available</strong>
 
-            <?php endforeach; ?>
+            <?php endif; ?>
             
         </p>
 
